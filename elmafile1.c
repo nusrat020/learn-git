3a653787687566bb75833ece4f58b499ce6ef1a8
elma <nelma52545254@gmail.com>
2021-08-04 13:41:58 +0600

    elmafile1 and elmafile2 are modified and staged

--- elmafile1.c
+++ elmafile1.c
@@ -3,6 +3,7 @@ int main()
 {
     printf("Good Morning");
     printf("Another good  day");
+    printf("the weather is nice");
 
     return 0;
 }
--- elmafile2.c
+++ elmafile2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    printf("File2");
+    int a, b ;
 
     return 0;
 }
